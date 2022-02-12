24e718b412671427f1671541d2d485cf62b87457
Shawn Graham <shawn.graham@carleton.ca>
2022-02-12 13:29:53 -0500

    Update bot.R
    quick fix for bad images?

--- bot.R
+++ bot.R
@@ -27,7 +27,8 @@ shortlink <- vgd_LinksShorten(longUrl = liveLink)
 
 tweet <- paste(artifactNumber,generalDescription,contextFunction,shortlink, sep=' ')
 
-image <- paste0(artifactNumber,'.aa.cs.thumb.png')
+#image <- paste0(artifactNumber,'.aa.cs.thumb.png')
+image <- paste0(thumbnail)
 imageUrl <- paste0('http://source.techno-science.ca/artifacts-artefacts/images/', URLencode(image))
 #if http_error is true, then the image URL is broken
 if (http_error(imageUrl)){
